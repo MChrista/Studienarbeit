730f0e45716030811174f794b26c39a0d71b4a4a
Michael Christa <mic007.ch@t-online.de>
2014-11-21 14:28:44 +0100

    generate memory

--- C_Programming/paging.c
+++ C_Programming/paging.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
+
+static uint8_t memory[16777216]; //Array with 16MB
+
+
+uint8_t * generateMemory(){
+	for(int i=0;i<16777216;i++){
+		memory[i]=0;
+	}
+	return memory;
+}
+
 
 
 int main(){
+	uint8_t *p = generateMemory();
 	printf("Hello World\n");
+	printf("%d",*(p+1));
 	return 0;
 }
