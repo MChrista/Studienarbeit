a8fa7c8cc1e2ff598ac3a9fc60c7a3fae04ab9af
Michael Christa <mic007.ch@t-online.de>
2014-11-22 11:02:05 +0100

    addressberechnung läuft, aber noch unsauber

--- C_Programming/mmu.c
+++ C_Programming/mmu.c
@@ -3,34 +3,37 @@
 #include <inttypes.h>
 
 // MMU Function
-int convertVirtualToPhysical( int virtualAddr, int* pageDir ) {
-  printf("Page Dir Address %08X\n",&pageDir);
+int convertVirtualToPhysical( int virtualAddr, int pageDir[] ) {
+  printf("Page Dir Address %08X\n",pageDir);
   int page_offset = virtualAddr & 0x00000FFF;
-  printf("Page Offset      : %08X\n", page_offset);
+  //printf("Page Offset      : %08X\n", page_offset);
   int page_table_offset = (virtualAddr & 0x003FF000) >> 12;
-  printf("Page Table Offset: %08X\n", page_table_offset);
+  //printf("Page Table Offset: %08X\n", page_table_offset);
   int page_dir_offset = virtualAddr >> 22;
-  printf("Page Dir Offset  : %08X\n", page_dir_offset);
+  //printf("Page Dir Offset  : %08X\n", page_dir_offset);
 
-  int page_table = pageDir + page_dir_offset; // vielleicht plus?
-  printf ("Page Dir Entry   : %08X\n", &page_table);
-  int page_entry = page_table + page_table_offset;
+  int * page_table = pageDir[1]; // vielleicht plus?
+  
+  printf ("Page Dir Entry   : %p\n", page_table);
+  int page_entry = * (page_table + page_table_offset);
   printf ("Page Table Entry : %08X\n", page_entry);
 
 
-  return  ( page_entry % 4096 ) + page_offset;
+  return  page_entry + page_offset;
 }
 
 int main() {
-  static int *pageDir[10];
+  static int pageDir[10];
   static int pageTable[10];
-
+  
+  printf("Address of Directory %p\n",pageDir);
+  printf("Address of pageTable %p\n",pageTable);
   pageDir[1] = & pageTable;
   pageTable[0] = 0x12345000;
  
   printf("Value of Directory: %08X\n", pageDir[1]);
   int testAddr = 0x00400CCC;
 
-  printf("%08X -> %08X\n", testAddr, convertVirtualToPhysical(testAddr, & pageDir));
+  printf("%08X -> %08X\n", testAddr, convertVirtualToPhysical(testAddr, pageDir));
 
 }
