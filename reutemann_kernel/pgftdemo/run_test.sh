dd869fb8feeb4a4e546bc421351559ecc09a1bf7
Jonas Polkehn <devjp@posteo.de>
2015-07-04 12:16:43 +0200

    Added test cases swap writeback segfault

--- reutemann_kernel/pgftdemo/run_test.sh
+++ reutemann_kernel/pgftdemo/run_test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-testfiles=(copy_page.txt mapping.txt)
+testfiles=(copy_page.txt mapping.txt swap.txt writeback.txt 
+segfault.txt)
 
-rm -rf tests/TestResult_Kernel/*
+#rm -rf tests/TestResult_Kernel/*
 
 for i in ${testfiles[@]}
 do
-./run_serial.sh < tests/Testfiles_Kernel/$i > tests/exspected_TestResult_Kernel/$i
-#./run_serial.sh < tests/Testfiles_Kernel/$i > tests/TestResult_Kernel/$i
+#./run_serial.sh < tests/Testfiles_Kernel/$i > 
+tests/exspected_TestResult_Kernel/$i
+./run_serial.sh < tests/Testfiles_Kernel/$i > tests/TestResult_Kernel/$i
 done
 diff -uqr tests/TestResult_Kernel/ tests/exspected_TestResult_Kernel/
 
