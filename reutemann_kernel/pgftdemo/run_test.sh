8664ac5171e1b5be2b4f3f87c4e796e68ba3f97b
Michael Christa <michael-christa@t-online.de>
2015-07-08 21:04:02 +0200

    Final Version

--- reutemann_kernel/pgftdemo/run_test.sh
+++ reutemann_kernel/pgftdemo/run_test.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-testfiles=(copy_page.txt mapping.txt simple_replace.txt writeback.txt 
-swap.txt segfault.txt)
+testfiles=(copy_page.txt mapping.txt simple_replace_test.txt writeback.txt swap.txt segfault.txt extended_replace_test.txt statetest.txt)
 
 rm tests/TestResult_Kernel/*
 
 for i in ${testfiles[@]}
 do
-#./run_serial.sh < tests/Testfiles_Kernel/$i > 
-#tests/exspected_TestResult_Kernel/$i
+#./run_serial.sh < tests/Testfiles_Kernel/$i > tests/exspected_TestResult_Kernel/$i
 ./run_serial.sh < tests/Testfiles_Kernel/$i > tests/TestResult_Kernel/$i
 done
 diff -uqr tests/TestResult_Kernel/ tests/exspected_TestResult_Kernel/
